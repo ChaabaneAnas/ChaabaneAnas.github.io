df104212aeee3eb121c9c6a9a60ce1c2e5ee2060
Anas Chaabane <anas.chaabane98@gmail.com>
2025-12-10 16:56:26 +0100

    fix: refactor next.config.mjs to use TypeScript types and improve structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
