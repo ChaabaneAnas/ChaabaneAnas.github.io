cfa5638fb19a705314f95e274a1f0153921ae31f
Anas Chaabane <anas.chaabane98@gmail.com>
2025-12-11 09:44:49 +0100

    fix: update ThemeToggle component to use outline variant and include SunMoon icon

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
-import { Sun, Moon } from "lucide-react";
+import { Sun, Moon, SunMoon } from "lucide-react";
 
 export default function ThemeToggle() {
   const { setTheme, theme } = useTheme();
@@ -17,11 +17,13 @@ export default function ThemeToggle() {
   if (!mounted) {
     return (
       <Button
-        variant="ghost"
+        variant="outline"
         size="icon"
-        className="h-8 w-8 rounded-lg bg-accent/70 text-accent-foreground hover:bg-accent/90 transition-colors"
+        className="border border-accent/30 text-accent/70 bg-card/50 dark:text-foreground dark:border-foreground/30 dark:hover:text-accent  dark:hover:border-accent/30 "
         aria-label="Toggle theme"
-      />
+      >
+        <SunMoon />
+      </Button>
     );
   }
 
