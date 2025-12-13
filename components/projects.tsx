844fe6299c16318d41a803c042e9f114786e5e68
Anas Chaabane <anas.chaabane98@gmail.com>
2025-12-13 20:07:00 +0100

    feat: implement Drawer component and update Projects for responsive design

--- components/projects.tsx
+++ components/projects.tsx
@@ -13,7 +13,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Label } from "@/components/ui/label";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 interface Project {
   id: string;
@@ -97,6 +108,7 @@ const projects: Project[] = [
 ];
 
 export default function Projects() {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   return (
     <section
       id="work"
@@ -109,115 +121,139 @@ export default function Projects() {
 
         {/* Projects Grid */}
         <div className="grid md:grid-cols-3 gap-8">
-          {projects.map((project) => (
-            <Dialog key={project.id}>
-              <DialogTrigger className="cursor-pointer group">
-                <div className="relative overflow-hidden rounded-xl mb-4 h-48 bg-muted ">
-                  <Image
-                    src={project.image || "/placeholder.svg"}
-                    alt={project.title}
-                    fill
-                    className="object-cover group-hover:scale-105 transition-transform duration-300"
-                  />
-                  <div className="absolute inset-0 bg-linear-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity" />
-                </div>
-                <h3 className="font-bold text-lg mb-2 group-hover:text-accent transition-colors">
-                  {project.title}
-                </h3>
-                <p className="text-foreground/60 text-sm mb-3">
-                  {project.description}
-                </p>
-                <div className="flex flex-wrap gap-2">
-                  {project.tech.slice(0, 2).map((tech) => (
-                    <span
-                      key={tech}
-                      className="px-2 py-1 text-xs rounded-full bg-accent/10 text-accent border border-accent/20"
-                    >
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-2xl">
-                <div className="h-70 relative">
-                  <Image
-                    src={project.image || "/placeholder.svg"}
-                    alt={project.title}
-                    fill
-                    className="rounded-md mb-4 object-cover w-full"
-                  />
-                </div>
-                <DialogHeader>
-                  <DialogTitle>{project.title}</DialogTitle>
-                  <DialogDescription>{project.summary}</DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4">
-                  <div className="grid gap-3">
-                    <Label>Outcome</Label>
-                    <p className="text-foreground/70">{project.outcome}</p>
-                  </div>
-                  <div className="grid gap-3">
-                    <Label>Tech Stack</Label>
+          {projects.map((project) => {
+            if (isDesktop) {
+              return (
+                <Dialog key={project.id}>
+                  <DialogTrigger className="cursor-pointer group">
+                    <div className="relative overflow-hidden rounded-xl mb-4 h-48 bg-muted ">
+                      <Image
+                        src={project.image || "/placeholder.svg"}
+                        alt={project.title}
+                        fill
+                        className="object-cover group-hover:scale-105 transition-transform duration-300"
+                      />
+                      <div className="absolute inset-0 bg-linear-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity" />
+                    </div>
+                    <h3 className="font-bold text-lg mb-2 group-hover:text-accent transition-colors">
+                      {project.title}
+                    </h3>
+                    <p className="text-foreground/60 text-sm mb-3">
+                      {project.description}
+                    </p>
                     <div className="flex flex-wrap gap-2">
-                      {project.tech.map((tech) => (
+                      {project.tech.slice(0, 2).map((tech) => (
                         <span
                           key={tech}
-                          className="px-3 py-1 text-sm rounded-full bg-accent/10 text-accent border border-accent/20"
+                          className="px-2 py-1 text-xs rounded-full bg-accent/10 text-accent border border-accent/20"
                         >
                           {tech}
                         </span>
                       ))}
                     </div>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-2xl">
+                    <div className="h-70 relative">
+                      <Image
+                        src={project.image || "/placeholder.svg"}
+                        alt={project.title}
+                        fill
+                        className="rounded-md mb-4 object-cover w-full"
+                      />
+                    </div>
+                    <DialogHeader>
+                      <DialogTitle>{project.title}</DialogTitle>
+                      <DialogDescription>{project.summary}</DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4">
+                      <div className="grid gap-3">
+                        <Label>Outcome</Label>
+                        <p className="text-foreground/70">{project.outcome}</p>
+                      </div>
+                      <div className="grid gap-3">
+                        <Label>Tech Stack</Label>
+                        <div className="flex flex-wrap gap-2">
+                          {project.tech.map((tech) => (
+                            <span
+                              key={tech}
+                              className="px-3 py-1 text-sm rounded-full bg-accent/10 text-accent border border-accent/20"
+                            >
+                              {tech}
+                            </span>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button className="w-full bg-accent hover:bg-accent/90 text-accent-foreground">
+                          Close
+                        </Button>
+                      </DialogClose>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              );
+            }
+            return (
+              <Drawer modal={false} key={project.id}>
+                <DrawerTrigger className="cursor-pointer group">
+                  <div className="relative overflow-hidden rounded-xl mb-4 h-48 bg-muted ">
+                    <Image
+                      src={project.image || "/placeholder.svg"}
+                      alt={project.title}
+                      fill
+                      className="object-cover group-hover:scale-105 transition-transform duration-300"
+                    />
+                    <div className="absolute inset-0 bg-linear-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity" />
                   </div>
-                </div>
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button className="w-full bg-accent hover:bg-accent/90 text-accent-foreground">
-                      Close
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          ))}
+                  <h3 className="font-bold text-lg mb-2 group-hover:text-accent transition-colors">
+                    {project.title}
+                  </h3>
+                  <p className="text-foreground/60 text-sm mb-3">
+                    {project.description}
+                  </p>
+                  <div className="flex flex-wrap gap-2">
+                    {project.tech.slice(0, 2).map((tech) => (
+                      <span
+                        key={tech}
+                        className="px-2 py-1 text-xs rounded-full bg-accent/10 text-accent border border-accent/20"
+                      >
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
+                </DrawerTrigger>
+                <DrawerContent>
+                  <DrawerHeader>
+                    <DrawerTitle>{project.title}</DrawerTitle>
+                    <DrawerDescription>{project.summary}</DrawerDescription>
+                  </DrawerHeader>
+                  <div className="grid gap-4">
+                    <div className="grid gap-3">
+                      <Label>Outcome</Label>
+                      <p className="text-foreground/70">{project.outcome}</p>
+                    </div>
+                    <div className="grid gap-3">
+                      <Label>Tech Stack</Label>
+                      <div className="flex flex-wrap gap-2">
+                        {project.tech.map((tech) => (
+                          <span
+                            key={tech}
+                            className="px-3 py-1 text-sm rounded-full bg-accent/10 text-accent border border-accent/20"
+                          >
+                            {tech}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </DrawerContent>
+              </Drawer>
+            );
+          })}
         </div>
       </div>
     </section>
   );
 }
-
-// export function DialogDemo() {
-//   return (
-//     <Dialog>
-//       <form>
-//         <DialogTrigger asChild>
-//           <Button variant="outline">Open Dialog</Button>
-//         </DialogTrigger>
-//         <DialogContent className="sm:max-w-[425px]">
-//           <DialogHeader>
-//             <DialogTitle>Edit profile</DialogTitle>
-//             <DialogDescription>
-//               Make changes to your profile here. Click save when you&apos;re
-//               done.
-//             </DialogDescription>
-//           </DialogHeader>
-//           <div className="grid gap-4">
-//             <div className="grid gap-3">
-//               <Label htmlFor="name-1">Name</Label>
-//               <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-//             </div>
-//             <div className="grid gap-3">
-//               <Label htmlFor="username-1">Username</Label>
-//             </div>
-//           </div>
-//           <DialogFooter>
-//             <DialogClose asChild>
-//               <Button variant="outline">Cancel</Button>
-//             </DialogClose>
-//             <Button type="submit">Save changes</Button>
-//           </DialogFooter>
-//         </DialogContent>
-//       </form>
-//     </Dialog>
-//   );
-// }
